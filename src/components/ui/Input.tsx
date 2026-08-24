76050a0f34cdc366ed2ff67a0f3d0d131cf3a79d
awakenedarpit <awakenedarpit@gmail.com>
2026-08-24 22:19:05 +0530

    feat: add reusable input component

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,5 @@
+import type { InputHTMLAttributes } from 'react'
+
+export function Input({ className = '', ...props }: InputHTMLAttributes<HTMLInputElement>) {
+  return <input {...props} className={`w-full rounded-xl border border-black/10 bg-transparent px-4 py-2.5 text-sm outline-none transition placeholder:text-black/35 focus:border-black/30 focus:ring-2 focus:ring-black/5 dark:border-white/10 dark:placeholder:text-white/35 dark:focus:border-white/30 dark:focus:ring-white/5 ${className}`} />
+}
