d7629b73d36a7a71892b0433e9336db1ccb10bf0
awakenedarpit <awakenedarpit@gmail.com>
2026-08-24 23:43:30 +0530

    a11y: add global skip navigation link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <a
+          href="#main-content"
+          className="sr-only focus:not-sr-only focus:fixed focus:left-4 focus:top-4 focus:z-50 focus:rounded-lg focus:bg-black focus:px-4 focus:py-3 focus:text-sm focus:font-medium focus:text-white focus:outline-none focus:ring-2 focus:ring-black/40 focus:ring-offset-2 dark:focus:bg-white dark:focus:text-black dark:focus:ring-white/50 dark:focus:ring-offset-black"
+        >
+          Skip to main content
+        </a>
+        {children}
+      </body>
     </html>
   );
 }
